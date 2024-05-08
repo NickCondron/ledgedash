241221d743034c32e869235325b4db92b007d90e
Nick Condron <ncondron42@gmail.com>
2024-05-08 13:01:44 -0400

    Create replay drop/upload component

--- src/components/sidebar.rs
+++ src/components/sidebar.rs
@@ -28,7 +28,7 @@ pub fn SideBar() -> Element {
                         }
                         li {
                             Link {
-                                to: Route::Wavedash {},
+                                to: Route::Technique {},
                                 class: "text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold",
                                 Icon {
                                     icon: bs_icons::BsBrush,
--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,13 @@ use dioxus::{
 use tracing::Level;
 
 mod components {
-    pub mod button;
+    pub mod replay_drop;
     pub mod sidebar;
 }
+use components::replay_drop::ReplayDrop;
 use components::sidebar::SideBar;
 
 const _TAILWIND_URL: &str = manganis::mg!(file("public/tailwind.css"));
-// const _STYLE: &str = {
-//     const _: &dyn manganis::ForMgMacro = {
-//         use manganis::*;
-//         &file("public/tailwind.css")
-//     };
-//     "/tailwindcssfe277c00ea35019e.css"
-// };
 
 #[derive(Clone, Routable, Debug, PartialEq)]
 #[rustfmt::skip]
@@ -27,8 +21,8 @@ enum Route {
     #[layout(SideBar)]
         #[route("/")]
         Home {},
-        #[route("/wavedash")]
-        Wavedash {},
+        #[route("/technique")]
+        Technique {},
 }
 
 fn main() {
@@ -51,66 +45,17 @@ fn App() -> Element {
 }
 
 #[component]
-fn Wavedash() -> Element {
+fn Technique() -> Element {
     rsx! {
-        div {
-            class: "flex items-center justify-center h-full",
-            "wavedash info"
-        }
+        p { "technique info" }
     }
 }
 
 #[component]
 fn Home() -> Element {
-    let mut search_subdirs = use_signal(|| true);
-    let mut num = use_signal(|| 0usize);
-    let mut name = use_signal(|| None as Option<String>);
-
-    let get_replays = move |evt: FormEvent| async move {
-        let Some(file_engine) = evt.files() else {
-            panic!()
-        };
-        let files = file_engine.files();
-        name.set(files.get(0).cloned());
-        num.set(files.len());
-    };
-
     rsx! {
-        h2 { "{num}" }
-        if let Some(name) = name() {
-            h2 { {name} }
-        }
-        div {
-            class: "grid grid-cols-2",
-            div {
-                label { r#for: "upload_replays", "Upload .slp replay files" }
-                input {
-                    class: "block",
-                    r#type: "file",
-                    accept: ".slp",
-                    multiple: true,
-                    name: "upload_replays",
-                    onchange: get_replays,
-                }
-            }
-            div {
-                label { r#for: "upload_replay_folder", "Upload folder containing replays" }
-                input {
-                    class: "block",
-                    r#type: "file",
-                    directory: true,
-                    name: "upload_replay_folder",
-                    onchange: get_replays,
-                }
-                label { r#for: "search_subdirs", "Search Subdirectories" }
-                input {
-                    class: "block",
-                    r#type: "checkbox",
-                    id: "search_subdirs",
-                    checked: search_subdirs,
-                    oninput: move |evt| search_subdirs.set(evt.checked()),
-                }
-            }
+        ReplayDrop {
+            on_slp: move |rp| println!("received {}", rp)
         }
     }
 }
